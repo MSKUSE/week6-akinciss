7c92fe04e4fefe863c6344a8426a3932b60c1f1a
Simge Akıncı <148821014+akinciss@users.noreply.github.com>
2025-03-28 12:35:37 +0300

    Update TestMyDate.java

--- src/TestMyDate.java
+++ src/TestMyDate.java
@@ -1,77 +1,98 @@
-public class TestMyDate {
 
-	public static void main(String[] args) {
-		MyDate date = new MyDate(28,2,2017);
-		
-		String strRep = date.toString();
-		System.out.println(strRep);
-		//Expected output: 2017-02-28
-		
-		date.incrementDay();
-		System.out.println(date.toString());
-		//Expected output: 2017-03-01
-		
-		
-		date.incrementYear(3);
-		System.out.println(date.toString());
-		//Expected output: 2020-03-01
-		
-		date.decrementDay();
-		System.out.println(date.toString());
-		//Expected output: 2020-02-29
-		
-		date.decrementYear();
-		System.out.println(date.toString());
-		//Expected output: 2019-02-28
-		
-		date.decrementMonth();
-		System.out.println(date.toString());
-		//Expected output: 2019-01-28		
+public class Shape {
 
-		
-		date.incrementDay(3);
-		System.out.println(date.toString());
-		//Expected output: 2019-01-31		
-		
-		date.decrementMonth(2);
-		System.out.println(date.toString());
-		//Expected output: 2018-11-30		
-		
-		date.decrementDay(30);
-		System.out.println(date.toString());
-		//Expected output: 2018-10-31			
+    private Point location;
 
-		date.incrementMonth(16);
-		System.out.println(date.toString());
-		//Expected output: 2020-02-29		
-	
-		date.decrementYear(4);
-		System.out.println(date.toString());
-		//Expected output: 2016-02-29
-	
-		date.incrementMonth();
-		System.out.println(date.toString());
-		//Expected output: 2016-03-29		
+    public Shape(Point location) {
+        this.location = location;
+    }
 
-		date.incrementYear();
-		System.out.println(date.toString());
-		//Expected output: 2017-03-29		
-		
-		MyDate anotherDate = new MyDate(28,2,2017);
-		
-		boolean before = date.isBefore(anotherDate);
-		System.out.println(date.toString()+ " is before " + anotherDate.toString()+ " : " + before);
-		//Expected output: 2017-03-29 is before 2017-02-28 : false		
-		
-		boolean after = date.isAfter(anotherDate);
-		System.out.println(date.toString()+ " is after " + anotherDate.toString()+ " : " + after);
-		//Expected output: 2017-03-29 is after 2017-02-28 : true
-		
-		int dayDiff = date.dayDifference(anotherDate);
-		System.out.println("Day difference between " + date.toString()+ " and " + anotherDate.toString()+ " is " + dayDiff);
-		//Expected output: Day difference between 2017-03-29 and 2017-02-28 is 29
-			
+    public Point getLocation() {
+        return location;
+    }
 
-	}
+    public void setLocation(Point location) {
+        this.location = location;
+    }
 
+    public double area(){
+        return 0;
+    }
+
+    public double perimeter(){
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "location=" + location +
+                '}';
+    }
+}
+public class Triangle extends Shape {
+
+    private int sideA, sideB, sideC;
+
+    public Triangle(Point sideA, int sideB, int sideC) {
+        super(sideA);
+        setSideB(sideB);
+        setSideC(sideC);
+    }
+
+    public int getSideA() {
+        return sideA;
+    }
+    public void setSideA(int sideA) {
+        if (sideA < 0){
+            this.sideA = 0;
+            throw new IllegalArgumentException("Side cant be negative");
+        }
+        else {
+            this.sideA = sideA;
+        }
+    }
+
+    public int getSideB() {
+        return sideB;
+    }
+    public void setSideB(int sideB) {
+        if (sideB < 0) {
+            this.sideB = 0;
+            System.out.println("Side B can't be negative!!");
+        } else {
+            this.sideB = sideB;
+        }
+    }
+
+    public int getSideC() {
+        return sideC;
+    }
+    public void setSideC(int sideC) {
+        if (sideC < 0) {
+            this.sideC = 0;
+            System.out.println("Side C can't be negative!!");
+        } else {
+            this.sideC = sideC;
+        }
+    }
+
+    @Override
+    public double area() {
+        return this.sideA*this.sideB/2;
+    }
+
+    @Override
+    public double perimeter() {
+        return this.sideA+this.sideB+this.sideC;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle{" +
+                "sideA=" + sideA +
+                ", sideB=" + sideB +
+                ", sideC=" + sideC +
+                '}';
+    }
 }
